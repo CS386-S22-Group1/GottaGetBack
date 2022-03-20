45338faaa861105d1e952d8364c2786269a2539d
Goofball <asajh4@gmail.com>
2022-03-20 12:29:03 -0700

    Mended the NetworkSpawner component to add wave controlled spawning in place of the Spawn Enemy button.

--- GottaGetBack/Assets/GameManagement/NetworkSpawner.cs
+++ GottaGetBack/Assets/GameManagement/NetworkSpawner.cs
@@ -1,21 +1,117 @@
 using Unity.Netcode;
 using UnityEngine;
 
-public class NetworkSpawner : NetworkBehaviour
+namespace EnemyManagement
 {
-    [SerializeField]
-    private NetworkObject networkSquarePrefab;
-
-    [SerializeField]
-    private Transform networkSpawnPoint;
-
-    public void SpawnEnemy()
+    public class NetworkSpawner : NetworkBehaviour
     {
-        if ( IsServer )
-        {
-            NetworkObject networkSquareInstance = Instantiate( networkSquarePrefab, networkSpawnPoint );
+        [Header( "WAVE SPAWNING INFORMATION" )]
 
-            networkSquareInstance.Spawn();
+        /// <summary>
+        ///     <para>
+        ///         Minimum number of enemies to be spawned in a wave
+        ///     </para>
+        /// </summary>
+        [SerializeField]
+        private int minEnemies = 1;
+
+        /// <summary>
+        ///     <para>
+        ///         Maximum number of enemies to be spawned in a wave
+        ///     </para>
+        /// </summary>
+        [SerializeField]
+        private int maxEnemies = 3;
+
+        /// <summary>
+        ///     <para>
+        ///         Time, in seconds, between the end of one wave, and the
+        ///         beginning of the next
+        ///     </para>
+        /// </summary>
+        [SerializeField]
+        private float timeBetweenWaves = 5.000000f;
+
+        [SerializeField]
+        private int enemiesLeftInWave = 0;
+
+
+        [Header( "ENEMY SPAWNING INFORMATION" )]
+
+        [SerializeField]
+        private NetworkObject networkEnemyPrefab;
+
+        [SerializeField]
+        private Transform[] networkSpawnPoints;
+
+
+        [Header( "TIMERS" )]
+
+        private float timeUntilNextWave = 0.000000f;
+
+
+        private void Start()
+        {
+            timeUntilNextWave = timeBetweenWaves;
+        }
+
+
+        private void Update()
+        {
+            // extra control to ensure that this object is only working on the
+            // server; this object should not be spawned in on any client instances
+            if ( !IsServer ) return;
+
+            if ( enemiesLeftInWave == 0 )
+            {
+                timeUntilNextWave -= Time.deltaTime;
+            }
+
+            if ( timeUntilNextWave <= 0.000000f )
+            {
+                InitializeWave();
+            }
+        }
+
+        /// <summary>
+        ///     <para>
+        ///         Spawns a random amount of enemies on the map, and sets
+        ///         enemiesLeftInWave
+        ///     </para>
+        /// </summary>
+        private void InitializeWave()
+        {
+            int numEnemiesToSpawn = Random.Range( minEnemies, maxEnemies );
+
+            int enemiesSpawned = 0;
+
+            while ( enemiesSpawned < numEnemiesToSpawn )
+            {
+                SpawnEnemy();
+            }
+
+            enemiesLeftInWave = enemiesSpawned + 1;
+
+            timeUntilNextWave = timeBetweenWaves;
+        }
+
+        /// <summary>
+        ///     <para>
+        ///         Chooses a spawn point, at random, to spawn an enemy at, and 
+        ///     </para>
+        /// </summary>
+        private void SpawnEnemy()
+        {
+            // choose an index in the networkSpawnPoints array
+            int chosenSpawnerIndex = Random.Range( 0, networkSpawnPoints.Length );
+
+            // save that spawner's transform
+            Transform chosenSpawnPoint = networkSpawnPoints[ chosenSpawnerIndex ];
+
+            NetworkObject networkEnemyInstance = Instantiate( networkEnemyPrefab, chosenSpawnPoint );
+
+            // Spawn this enemy object on all clients
+            networkEnemyInstance.Spawn();
         }
     }
 }
